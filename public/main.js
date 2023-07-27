028c83094b6b83af473c2b82848dcc907fa73495
jonatemps <mupene7@gmail.com>
2023-07-27 22:03:53 +0100

    first commit

---
+++ public/main.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "make:component":
+/*!**********************!*\
+  !*** make:component ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"make:component\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"make:\" URIs.\n    at C:\\laragon\\www\\example-app\\node_modules\\webpack\\lib\\NormalModule.js:834:25\n    at Hook.eval [as callAsync] (eval at create (C:\\laragon\\www\\example-app\\node_modules\\tapable\\lib\\HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (C:\\laragon\\www\\example-app\\node_modules\\tapable\\lib\\Hook.js:18:14)\n    at Object.processResource (C:\\laragon\\www\\example-app\\node_modules\\webpack\\lib\\NormalModule.js:831:8)\n    at processResource (C:\\laragon\\www\\example-app\\node_modules\\loader-runner\\lib\\LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (C:\\laragon\\www\\example-app\\node_modules\\loader-runner\\lib\\LoaderRunner.js:171:10)\n    at runLoaders (C:\\laragon\\www\\example-app\\node_modules\\loader-runner\\lib\\LoaderRunner.js:398:2)\n    at NormalModule._doBuild (C:\\laragon\\www\\example-app\\node_modules\\webpack\\lib\\NormalModule.js:821:3)\n    at NormalModule.build (C:\\laragon\\www\\example-app\\node_modules\\webpack\\lib\\NormalModule.js:965:15)\n    at C:\\laragon\\www\\example-app\\node_modules\\webpack\\lib\\Compilation.js:1371:12");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["make:component"]();
+/******/ 	
+/******/ })()
+;
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    @livewireStyles
+  </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-dark bg-dark fixed-top">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">SD-FOCUS</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header">
+                  <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+                  <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                  <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">Monitoring</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">First-Call / Frist-Call Qualité</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Performances
+                      </a>
+                      <ul class="dropdown-menu dropdown-menu-dark">
+                        <li><a class="dropdown-item" href="#">Infra</a></li>
+                        <li><a class="dropdown-item" href="#">Distribution</a></li>
+                        {{-- <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li> --}}
+                      </ul>
+                    </li>
+                  </ul>
+                  <form class="d-flex mt-3" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-success" type="submit">Search</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </nav>
+
+    </header>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    @livewireScripts
+  </body>
+</html>
